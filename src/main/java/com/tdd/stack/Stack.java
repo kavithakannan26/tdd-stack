0cb4571b8350077776ab5f4faa55bdc86d0ee793
Kavitha Kannan <kavitha.Kannan@wolterskluwer.com>
2017-09-29 08:39:10 -0400

    Kata#TDD - KK - all readme requirements done

--- src/main/java/com/tdd/stack/Stack.java
+++ src/main/java/com/tdd/stack/Stack.java
@@ -1,5 +1,34 @@
 package com.tdd.stack;
 
-public class Stack {
+import java.util.ArrayList;
 
+class Stack<T> {
+
+    ArrayList<T> storedStack = new ArrayList<>();
+
+    int size() {
+        return this.storedStack.size();
+    }
+
+    void push(final T i) {
+        this.storedStack.add(i);
+    }
+
+    T pop() {
+        if (this.storedStack.size() == 0)
+        {
+            throw new IndexOutOfBoundsException();
+        }
+        final int i = this.storedStack.size();
+        return this.storedStack.remove(i-1);
+
+    }
+
+    T peek() {
+        return this.storedStack.get(this.storedStack.size()-1);
+    }
+
+    boolean isEmpty() {
+        return this.storedStack.isEmpty();
+    }
 }
--- src/test/java/com/tdd/stack/StackTest.java
+++ src/test/java/com/tdd/stack/StackTest.java
@@ -1,5 +1,78 @@
 package com.tdd.stack;
 
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.*;
+import static org.junit.Assert.*;
+
 public class StackTest {
 
+    @Test
+    public void shouldGetEmptyStackSize() throws Exception {
+        final Stack<Integer> stack = new Stack<>();
+        assertThat(stack.size(), is(0));
+        assertThat(stack.storedStack.size(), is(0));
+    }
+
+    @Test
+    public void shouldPushOne() throws Exception {
+        final Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        assertThat(stack.storedStack.size(), is(1));
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void shouldPopOne() throws Exception {
+        final Stack<Integer> stack = new Stack<>();
+        stack.pop();
+    }
+
+    @Test
+    public void shouldPushTwo() throws Exception {
+        final Stack<Integer> stack = new Stack<>();
+        stack.push(2);
+        stack.push(4);
+        assertThat(stack.storedStack.size(), is(2));
+    }
+
+    @Test
+    public void shouldPop() throws Exception {
+        final Stack<Integer> stack = new Stack<>();
+        stack.push(2);
+        stack.push(5);
+        final int valuePopped = stack.pop();
+        assertThat(stack.storedStack.size(), is(1));
+        assertThat(valuePopped, is(5));
+
+    }
+
+    @Test
+    public void shouldPeekOne() throws Exception {
+        final Stack<Integer> stack = new Stack<>();
+        stack.push(5);
+        stack.push(7);
+        final int valuePeeked = stack.peek();
+        assertThat(valuePeeked, is(7));
+    }
+
+    @Test
+    public void shouldSayIsEmpty() throws Exception {
+        final Stack<Integer> stack = new Stack<>();
+        assertTrue(stack.isEmpty());
+    }
+
+    @Test
+    public void shouldSayIsNotEmpty() throws Exception {
+        final Stack<Integer> stack = new Stack<>();
+        stack.push(986);
+        assertFalse(stack.isEmpty());
+    }
+
+    @Test
+    public void shouldPeekString() throws Exception {
+        final Stack<String> stack = new Stack<>();
+        stack.push("Apple");
+        final String valuePeeked = stack.peek();
+        assertThat(valuePeeked, is("Apple"));
+    }
 }
